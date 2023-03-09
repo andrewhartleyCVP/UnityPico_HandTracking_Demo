6c69da5a3f5139e6abcb8955db5df48d95727c3b
Danny.Saetern <pico_devsupport@bytedance.com>
2023-03-09 05:28:16 -0800

    Update HandClickCtrl.cs
    Added comments and rearranged methods

--- Assets/Features/HandTrackingDemo/Scripts/HandClickCtrl.cs
+++ Assets/Features/HandTrackingDemo/Scripts/HandClickCtrl.cs
@@ -62,58 +62,61 @@ public class HandClickCtrl : MonoBehaviour
 
     
 
-    private void OnRayClickDown()
+    //We want to control where and when the user can shoot, add colors changes to the ray pointer for valid and invalid areas
+    //add a laser to help players aim when in valid area
+    private void UpdateRaycast()
     {
-        //Triggers when TouchStrengthRay >= clickDownRayStrength;
-        this.mCurClickState = RayClickState.Clicking;
-        if (!ArcadeManager.instance.CoinGrabbed)
+        mHitResult = Physics.Raycast(this.mPointPose.transform.position, this.mPointPose.transform.up, out mCurHitInfo, shootRange, shootZoneLayer);
+
+        if (mHitResult)
         {
-
-            if(canFire && reload && fireZone)
+            if (mCurHitInfo.collider.CompareTag("ZoneShoot"))
             {
-                ClickFire();
-                canFire= false;
-                reload= false;
-                laserLine.startColor = Color.red;
+                //we hit something
+                if (!fireZone)
+                {
+                    fireZone = true;
 
+                    rayMat.SetColor("_FresnelColor", Color.cyan);
+                    rayMat.SetFloat("_AlphaIntensity", .2f);
+                    rayMat.SetFloat("_FresnelPower", .9f);
+                }
+
+                if (fireZone && !ArcadeManager.instance.CoinGrabbed)
+                {
+                    if (!laserLine.enabled)
+                        laserLine.enabled = true;
+
+                    laserLine.SetPosition(0, mPointPose.transform.position);
+                    laserLine.SetPosition(1, mCurHitInfo.point);
+                }
+            }
+        }
+        else
+        {
+            if (fireZone)
+            {
+                fireZone = false;
+
+                rayMat.SetColor("_FresnelColor", Color.red);
+                rayMat.SetFloat("_AlphaIntensity", .1f);
+                rayMat.SetFloat("_FresnelPower", .2f);
+                laserLine.enabled = false;
             }
         }
     }
 
-    private void OnRayClickUp()
-    {
-        //Triggers when TouchStrengthRay <= clickUpRayStrength;
-        this.mCurClickState = RayClickState.None;
-        if (reload == false)
-        {
-            reload = true;
-            SoundManager.instance.PlayReload();
-        }
-    }
-
-    private void UpdateFireDelay()
-    {
-        if (canFire == false)
-        {
-            timeToShoot += Time.deltaTime;
-            if (timeToShoot >= fireDelay)
-            {
-                timeToShoot = 0;
-                canFire = true;
-                laserLine.startColor = Color.cyan;
-            }
-        }
-    }
-
+    
     private void UpdateHandTracking()
     {
         
         if (PXR_HandTracking.GetActiveInputDevice() != ActiveInputDevice.HandTrackingActive)
             return;
 
+        //Ray pointer is valid when it appears on the users hand
         if (pxrHand.RayValid)
         {
-
+            //Raytouched is true when TouchStrengthRay >= .7, otherwise it is false
             if (pxrHand.RayTouched && pxrHand.TouchStrengthRay >= clickDownRayStrength)
             {
                 if (this.mCurClickState == RayClickState.None)
@@ -142,50 +145,38 @@ public class HandClickCtrl : MonoBehaviour
         }
 
     }
-
-
-    private void UpdateRaycast()
+    private void OnRayClickUp()
     {
-        mHitResult = Physics.Raycast(this.mPointPose.transform.position, this.mPointPose.transform.up, out mCurHitInfo, shootRange, shootZoneLayer);
-
-        if (mHitResult)
+        //Triggers when TouchStrengthRay <= clickUpRayStrength;
+        this.mCurClickState = RayClickState.None;
+        if (reload == false)
         {
-            if(mCurHitInfo.collider.CompareTag("ZoneShoot"))
-            {
-                //we hit something
-                if (!fireZone)
-                {
-                    fireZone = true;
-
-                    rayMat.SetColor("_FresnelColor", Color.cyan);
-                    rayMat.SetFloat("_AlphaIntensity", .2f);
-                    rayMat.SetFloat("_FresnelPower", .9f);
-                }
-
-                if(fireZone && !ArcadeManager.instance.CoinGrabbed)
-                {
-                    if(!laserLine.enabled)
-                        laserLine.enabled = true;
-
-                    laserLine.SetPosition(0, mPointPose.transform.position);
-                    laserLine.SetPosition(1, mCurHitInfo.point);
-                }
-            }
-        }
-        else
-        {
-            if(fireZone)
-            {
-                fireZone= false;
-
-                rayMat.SetColor("_FresnelColor", Color.red);
-                rayMat.SetFloat("_AlphaIntensity", .1f);
-                rayMat.SetFloat("_FresnelPower", .2f);
-                laserLine.enabled = false;
-            }
+            reload = true;
+            SoundManager.instance.PlayReload();
         }
     }
 
+    //User must have reloaded, be pointing at the firezone area and have waited past his delay time
+    private void OnRayClickDown()
+    {
+        //Triggers when TouchStrengthRay >= clickDownRayStrength;
+        this.mCurClickState = RayClickState.Clicking;
+        if (!ArcadeManager.instance.CoinGrabbed)
+        {
+            //when all conditions are met, let the user fire
+            if (canFire && reload && fireZone)
+            {
+                ClickFire();
+                canFire = false;
+                reload = false;
+                laserLine.startColor = Color.red;
+
+            }
+        }
+    }
+    
+    //Fire from the raypointer
+ 
     private void ClickFire()
     {
         if (bulletPrefab == null)
@@ -196,5 +187,20 @@ public class HandClickCtrl : MonoBehaviour
         Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
         bulletRB.AddForce(mPointPose.transform.up * bulletForce, ForceMode.Impulse);
         SoundManager.instance.PlayShootBullet();
+    }
+
+    //Delay the user from firing too fast
+    private void UpdateFireDelay()
+    {
+        if (canFire == false)
+        {
+            timeToShoot += Time.deltaTime;
+            if (timeToShoot >= fireDelay)
+            {
+                timeToShoot = 0;
+                canFire = true;
+                laserLine.startColor = Color.cyan;
+            }
+        }
     }
 }
